1d7be9864fbcacb731fee91612b01b4e167ecfa2
15138730370 <1037483576@qq.com>
2018-01-03 08:04:12 +0800

    save to back

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
-import { QrcodeComponent } from './qrcode/qrcode';
-import { MeepoCoreServiceModule } from 'meepo-core';
-import { UuidModule } from 'meepo-uuid';
 
 @NgModule({
     imports: [
-        MeepoCoreServiceModule,
-        UuidModule
     ],
-    exports: [QrcodeComponent],
-    declarations: [QrcodeComponent],
+    exports: [],
+    declarations: [],
     providers: [],
 })
 export class QrcodeModule { }
-export { QrcodeComponent } from './qrcode/qrcode';
 
